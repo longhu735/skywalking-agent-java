81abb741b84d29361e90cd2975caa1669d74eb22
yangguo <yangguo@caschina.cn>
2025-04-27 15:06:01 +0800

    增加配置文件和修改sw_cxt内容

--- apm-sniffer/apm-toolkit-activation/apm-toolkit-logging-common/src/main/java/org/apache/skywalking/apm/toolkit/logging/common/log/SkyWalkingContext.java
+++ apm-sniffer/apm-toolkit-activation/apm-toolkit-logging-common/src/main/java/org/apache/skywalking/apm/toolkit/logging/common/log/SkyWalkingContext.java
@@ -49,8 +49,10 @@ public class SkyWalkingContext {
     @Override
     public String toString() {
         if (-1 == spanId) {
-            return "[" + String.join(",", serviceName, instanceName, "N/A", "N/A", "-1") + "]";
+//            return "[" + String.join(",", serviceName, instanceName, "N/A", "N/A", "-1") + "]";
+            return "[serviceName：" + serviceName + ",traceId:N/A,traceSegmentId:N/A,spanId:-1]";
         }
-        return "[" + String.join(",", serviceName, instanceName, traceId, traceSegmentId, String.valueOf(spanId)) + "]";
+//        return "[" + String.join(",", serviceName, instanceName, traceId, traceSegmentId, String.valueOf(spanId)) + "]";
+        return "[serviceName：" + serviceName + ",traceId:" + traceId + ",traceSegmentId:" + traceSegmentId + ",spanId:" + spanId + "]";
     }
 }
